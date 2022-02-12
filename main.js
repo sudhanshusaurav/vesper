51b8b6f854d8dd1a0b64addf8b07bfc3276d9781
sudhanshu_saurav <sudhanshussaurav@gmail.com>
2022-02-12 21:43:30 +0530

    responsiveness

--- main.js
+++ main.js
@@ -6,38 +6,6 @@ $(document).ready(function () {
     // centerPadding: '60px',
     infinite: true,
     arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          infinite: true,
-          arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ],
   });
 
   $('.apartment-arrow-right').click(function () {
@@ -190,7 +158,7 @@ $(document).ready(function () {
     duration: 1,
     scrollTrigger: { trigger: '.interior', start: 'top 80%' },
   });
-  gsap.from('.interior__text', {
+  gsap.from('.interiors__text', {
     y: 200,
     opacity: 0,
     duration: 1,
