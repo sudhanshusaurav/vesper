d3ee24fae539011e54e242c3a47dde1217f62d22
sudhanshu_saurav <sudhanshussaurav@gmail.com>
2022-02-12 19:19:10 +0530

    fix

--- main.js
+++ main.js
@@ -6,6 +6,38 @@ $(document).ready(function () {
     // centerPadding: '60px',
     infinite: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          infinite: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
 
   $('.apartment-arrow-right').click(function () {
